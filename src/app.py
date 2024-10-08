8de2231f387d8708d9004140c0d4257738384faf
Miguel Navas <143790463+menr-25@users.noreply.github.com>
2024-10-08 08:13:01 +0000

    FIRST API DONE

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+   {},
+]
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    json_text = jsonify(todos), 200
+    return json_text
+
+@app.route("/todos", methods=["POST"])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    return jsonify([todos[len(todos) - 1], todos]), 201
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    todos.pop(position)
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
